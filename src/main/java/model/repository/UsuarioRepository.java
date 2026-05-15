07802270b4854db4612834472e388ba785cd7b91
Lucas Beda <timoteolucas665@gmail.com>
2026-05-15 18:29:49 -0300

    criação usuario repository

--- src/main/java/model/repository/UsuarioRepository.java
+++ src/main/java/model/repository/UsuarioRepository.java
@@ -1,4 +1,33 @@
 package model.repository;
 
+import model.entity.Usuario;
+import java.util.HashMap;
+
 public class UsuarioRepository {
+    private HashMap<Integer, Usuario> usuarios = new HashMap<>();
+    private int proximoId = 1;
+
+    public void salvar(Usuario usuario) {
+        usuario.setId(proximoId++);
+        usuarios.put(usuario.getId(), usuario);
+    }
+
+    public Usuario buscarPorId(int id) {
+        return usuarios.get(id);
+    }
+
+    public HashMap<Integer, Usuario> listarTodos() {
+        return usuarios;
+    }
+
+    public boolean remover(int id) {
+        return usuarios.remove(id) != null;
+    }
+
+    public boolean cpfJaExiste(String cpf) {
+        for (Usuario u : usuarios.values()) {
+            if (u.getCpf().equals(cpf)) return true;
+        }
+        return false;
+    }
 }

f13d5033ca037f937b5693c81fd1e2a049cd2437
Arthur Pagliarini Martins <arthur_p_martins@estudante.sesisenai.org.br>
2026-05-15 18:47:29 -0300

    Criação reserva repository

--- src/main/java/model/repository/ReservaRepository.java
+++ src/main/java/model/repository/ReservaRepository.java
@@ -1,4 +1,31 @@
 package model.repository;
 
+import model.entity.*;
+import java.util.*;
+
 public class ReservaRepository {
+    private HashMap<Integer, Reserva> reservas = new HashMap<>();
+    private int proximoId = 1;
+
+    public void salvar(Reserva reserva) {
+        reserva.setId(proximoId++);
+        reservas.put(reserva.getId(), reserva);
+    }
+
+    public Reserva buscarPorId(int id) {
+        return reservas.get(id);
+    }
+
+    public HashMap<Integer, Reserva> listarTodos() {
+        return reservas;
+    }
+
+    public boolean remover(int id) {
+        return reservas.remove(id) != null;
+    }
+
+
+
+
+
 }
